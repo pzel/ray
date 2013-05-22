5a0b17cfe7878f96c7e9acc6888ee61fba9ac27f
pzel <p@manisola.net>
2013-05-22 06:38:50 +0200

    fisheye removal

--- src/Main.hs
+++ src/Main.hs
@@ -13,11 +13,28 @@ main = do
   SDL.enableKeyRepeat 1 1
   SDL.setVideoMode G.screenWidth G.screenHeight 8 []
   screen <- SDL.getVideoSurface
-  let level = mkLevel [[Wall,Wall,Wall,Wall],
+  let level = mkLevel [[Wall, Wall, Wall,Wall],
                        [Wall,Empty,Empty,Wall],
                        [Wall,Empty,Empty,Wall],
-                       [Wall,Wall,Wall,Wall]] -- mkSquareLevel 17
-  mainLoop screen level (65,65) 280
+                       [Wall,Empty,Empty,Wall],
+                       [Wall,Empty,Empty,Wall],
+                       [Wall,Empty,Empty,Wall],
+                       [Wall,Empty,Empty,Wall],
+                       [Wall,Empty,Empty,Wall],
+                       [Wall,Empty,Empty,Wall],
+                       [Wall,Empty,Empty,Wall],
+                       [Wall,Empty,Empty,Wall],
+                       [Wall,Empty,Empty,Wall],
+                       [Wall,Empty,Empty,Wall],
+                       [Wall,Empty,Empty,Empty,Wall, Wall, Wall, Wall],
+                       [Wall,Empty,Empty,Empty,Empty,Empty,Empty,Wall],
+                       [Wall,Empty,Empty,Empty,Empty,Empty,Empty,Wall],
+                       [Wall,Empty,Empty,Empty,Empty,Empty,Empty,Wall],
+                       [Wall,Empty,Empty,Empty,Wall, Wall, Wall, Wall],
+                       [Wall,Empty,Empty,Wall],
+                       [Wall,Empty,Empty,Wall],
+                       [Wall, Wall, Wall,Wall]] 
+  mainLoop screen level (65,65) 45
   SDL.quit
  where
   mainLoop s l p@(px,py) a = do
@@ -26,8 +43,8 @@ main = do
    e <- SDL.pollEvent
    case e of
     (KeyDown (Keysym SDLK_q _ _))    -> return ()
-    (KeyDown (Keysym SDLK_UP _ _)) -> mainLoop s l (moveByAngle l p a) a
-    (KeyDown (Keysym SDLK_DOWN _ _)) -> mainLoop s l (moveByAngle l p (deg(a+180.0))) a
+    (KeyDown (Keysym SDLK_UP _ _)) -> mainLoop s l (moveByAngle 7 l p a) a
+    (KeyDown (Keysym SDLK_DOWN _ _)) -> mainLoop s l (moveByAngle 7 l p (deg(a+180.0))) a
     (KeyDown (Keysym SDLK_LEFT _ _)) -> mainLoop s l p (deg (a-3.0))
     (KeyDown (Keysym SDLK_RIGHT _ _)) -> mainLoop s l p (deg (a+3.0))
     _                              -> mainLoop s l p a
@@ -42,13 +59,18 @@ castRays :: Surface -> Level -> LevelPos -> Angle -> IO ()
 castRays s l lp facing =
  do
    let angles = take G.pPlaneW $ iterate (\a->deg(G.pPlaneColWidth+a)) (facing - G.halfFov)
-       distances = map (castRay l lp lp) angles -- unfishbowlize
-   rects <- zipWithM projection distances [0..]
+       distances = map (castRay l lp lp) angles
+       correctedDistances = (zipWith (correct facing) angles distances)
+   rects <- zipWithM projection correctedDistances [0..]
    mapM_ (\r-> SDL.fillRect s r (Pixel 12566463)) rects
    return ()
 
+correct :: Angle -> Angle -> Float -> Float
+correct facing ray dist = dist * (cos' (deg (ray - facing)))
+
+
 projection :: Float -> Int -> IO (Maybe Rect)
-projection d col =
+projection  d col =
   let height = (round $ (fromIntegral G.blockSize / d) * 277)::Int
       top = max 0 $ (G.pPlaneH `div` 2) - (height `div` 2)
   in return $ Just $ Rect col top 1 (height)
@@ -58,12 +80,12 @@ castRay l p@(px,py) c@(cx,cy) a =
   if newC == c
   then getDistance p c
   else castRay l p newC a
- where newC = moveByAngle l c a 
+ where newC = moveByAngle 4 l c a 
 
-moveByAngle :: Level -> LevelPos -> Angle -> LevelPos
-moveByAngle l p@(px,py) a = 
-  let dx = (cos' a) * 3
-      dy = (sin' a) * 3
+moveByAngle :: Float -> Level -> LevelPos -> Angle -> LevelPos
+moveByAngle s l p@(px,py) a = 
+  let dx = (cos' a) * s
+      dy = (sin' a) * s
       newP = (px+dx,py+dy)
   in if (isWall l newP) then p else newP
 
